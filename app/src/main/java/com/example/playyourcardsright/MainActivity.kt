a8002a854f817766e747e6dcdad460dffca94b2e
Shahash Kandel <shahashkandel@gmail.com>
2024-11-20 13:26:46 -0500

    binded with the root

--- app/src/main/java/com/example/playyourcardsright/MainActivity.kt
+++ app/src/main/java/com/example/playyourcardsright/MainActivity.kt
@@ -6,19 +6,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.playyourcardsright.databinding.ActivityMainBinding
+import com.bignerdranch.android.photogallery.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-//        setContentView(R.layout.activity_main)
+
         val binding = ActivityMainBinding.inflate(layoutInflater)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
 
         binding.warButton.setOnClickListener {
             val intent = Intent(this, War::class.java)
