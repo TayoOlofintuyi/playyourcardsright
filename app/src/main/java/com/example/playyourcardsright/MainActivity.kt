c51842b9082e3bd082826c49250cfc6be76510a5
TayoOlofintuyi <tolofi152@gmail.com>
2024-11-20 11:45:01 -0500

    tried to add binding in main activity

--- app/src/main/java/com/example/playyourcardsright/MainActivity.kt
+++ app/src/main/java/com/example/playyourcardsright/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.playyourcardsright
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.playyourcardsright.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.warButton.setOnClickListener {
+            val intent = Intent(this, War::class.java)
+            startActivity(intent)
         }
     }
 }
