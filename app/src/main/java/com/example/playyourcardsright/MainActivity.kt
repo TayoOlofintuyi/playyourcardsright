bf46a61033ccb94191b15ccbfe13b31886d51760
Shahash Kandel <shahashkandel@gmail.com>
2024-11-20 13:55:13 -0500

    still working

--- app/src/main/java/com/example/playyourcardsright/MainActivity.kt
+++ app/src/main/java/com/example/playyourcardsright/MainActivity.kt
@@ -1,18 +1,13 @@
 package com.example.playyourcardsright
-
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.bignerdranch.android.photogallery.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
--- app/src/main/java/com/example/playyourcardsright/War.kt
+++ app/src/main/java/com/example/playyourcardsright/War.kt
@@ -1,24 +1,18 @@
 package com.example.playyourcardsright
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
-import com.example.playyourcardsright.api.DeckOfCardsApi
-import kotlinx.coroutines.launch
+import com.bignerdranch.android.photogallery.databinding.ActivityWarBinding
+
 
 class War : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_war)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val binding = ActivityWarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 
 //    val api = DeckOfCardsApi()
