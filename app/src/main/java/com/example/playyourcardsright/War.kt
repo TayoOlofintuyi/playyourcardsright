440e2e0108a21727980fb421eb8a86a393167603
Shahash Kandel <shahashkandel@gmail.com>
2024-11-22 15:29:27 -0500

    Added menu to view the history of the game

--- app/src/main/java/com/example/playyourcardsright/War.kt
+++ app/src/main/java/com/example/playyourcardsright/War.kt
@@ -1,6 +1,9 @@
 package com.example.playyourcardsright
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.playyourcardsright.databinding.ActivityWarBinding
@@ -13,6 +16,7 @@ class War : AppCompatActivity() {
         val binding = ActivityWarBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         binding.playButton.setOnClickListener {
             val openDeckRepository = DeckRepository()
             lifecycleScope.launch {
@@ -24,4 +28,22 @@ class War : AppCompatActivity() {
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_war, menu)
+        return true
+    }
+
+    // Handle menu item clicks
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.war_menu -> {
+                // Handle the action for viewing the game history
+                Log.d("War Activity", "View Results clicked")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
+
